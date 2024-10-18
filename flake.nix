38bb3af4c2f96cddaf5d9cd331eafe1c91369e4f
Emory Merryman IS 300 <viktordanek10@gmail.com>
2024-10-18 10:55:04 -0400

    YES

--- flake.nix
+++ flake.nix
@@ -1,34 +1,42 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        lib = name : builtins.concatStringsSep "" [ "$" "{" (builtins.toString name) "}" ];
-      in {
-        lib = lib;
-
-        checks.testLib = pkgs.stdenv.mkDerivation {
-          name = "test-lib";
-          builder = "${pkgs.bash}/bin/bash";
-          args = [ "-c" ''
-            set -e
-            result='${lib "myVar"}'  # Pass "myVar" to lib as a parameter.
-            expected='${ builtins.concatStringsSep "" [ "$" "{" "myVar" "}" ] }'
-
-            if [ "$result" != "$expected" ]; then
-              echo "Test failed: expected \"$expected\" but got \"$result\""
-              exit 1
-            else
-                ${ pkgs.coreutils }/bin/mkdir $out &&
-              echo "Test passed: $result"
-            fi
-          '' ];
-        };
-      }
-    );
+    inputs =
+        {
+            nixpkgs.url = "github:NixOS/nixpkgs" ;
+            flake-utils.url = "github:numtide/flake-utils" ;
+        } ;
+    outputs =
+        { self, nixpkgs, flake-utils, ... } :
+            let
+                fun =
+                    system :
+                    let
+                        pkgs = import nixpkgs { inherit system; } ;
+                        lib = name : builtins.concatStringsSep "" [ "$" "{" (builtins.toString name) "}" ] ;
+                        in
+                            {
+                                lib = lib ;
+                                checks.testLib =
+                                    pkgs.stdenv.mkDerivation
+                                        {
+                                            name = "test-lib";
+                                            builder = "${pkgs.bash}/bin/bash" ;
+                                            args =
+                                                [
+                                                    "-c"
+                                                    ''
+                                                        observed='${lib "myVar"}' &&
+                                                            expected='${ builtins.concatStringsSep "" [ "$" "{" "myVar" "}" ] }' &&
+                                                            if [ "$observed != "$expected" ]
+                                                            then
+                                                                ${ pkgs.coreutils }/bin/echo "Test failed: expected \"$expected\" but got \"$result\"" &&
+                                                                    exit 1
+                                                            else
+                                                                ${ pkgs.coreutils }/bin/mkdir $out &&
+                                                                    ${ pkgs.coreutils }/bin/echo "Test passed: $result"
+                                                            fi
+                                                    ''
+                                                ] ;
+                                        } ;
+                            } ;
+                in flake-utils.lib.eachDefaultSystem fun ;
 }
