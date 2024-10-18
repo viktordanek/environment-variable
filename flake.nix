5625d5b1107b7bb2e6b5552e7676beb715d2b94a
Emory Merryman IS 300 <viktordanek10@gmail.com>
2024-10-18 11:34:53 -0400

    does this work

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
     inputs =
         {
-            flake-utils.url = "github:numtide/flake-utils?rev=b1d9ab70662946ef0850d488da1c9019f3a9752a" ;
-            nixpkgs.url = "github:NixOs/nixpkgs?rev=9afce28a1719e35c295fe8b379a491659acd9cd6" ;
+            flake-utils.url = "github:numtide/flake-utils" ;
+            nixpkgs.url = "github:NixOs/nixpkgs" ;
         } ;
     outputs =
         { self, nixpkgs, flake-utils, ... } :
