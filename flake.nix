f0f911bf9a36a54bd14c85c60fb75bbd03b58157
Emory Merryman IS 300 <viktordanek10@gmail.com>
2024-10-18 09:01:17 -0400

    YES

--- flake.nix
+++ flake.nix
@@ -1,4 +1,34 @@
 {
-    inputs = { } ;
-    outputs = { self } : { lib = name : builtins.concatStringsSep "" [ "$" "{" ( builtins.toString name ) "}" ] ; } ;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = name : builtins.concatStringsSep "" [ "$" "{" (builtins.toString name) "}" ];
+      in {
+        lib = lib;
+
+        checks.testLib = pkgs.stdenv.mkDerivation {
+          name = "test-lib";
+          builder = "${pkgs.bash}/bin/bash";
+          args = [ "-c" ''
+            set -e
+            result='${lib "myVar"}'  # Pass "myVar" to lib as a parameter.
+            expected='${ builtins.concatStringsSep "" [ "$" "{" "myVar" "}" ] }'
+
+            if [ "$result" != "$expected" ]; then
+              echo "Test failed: expected \"$expected\" but got \"$result\""
+              exit 1
+            else
+                ${ pkgs.coreutils }/bin/mkdir $out &&
+              echo "Test passed: $result"
+            fi
+          '' ];
+        };
+      }
+    );
 }
