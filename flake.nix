6b51a482214423423651b93bf02995cc89204c51
Emory Merryman IS 300 <viktordanek10@gmail.com>
2024-10-18 11:36:13 -0400

    initialize

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+    inputs =
+        {
+            flake-utils.url = "github:numtide/flake-utils" ;
+            nixpkgs.url = "github:NixOs/nixpkgs" ;
+        } ;
+    outputs =
+        { self, nixpkgs, flake-utils, ... } :
+            let
+                fun =
+                    system :
+                    let
+                        pkgs = import nixpkgs { inherit system; } ;
+                        lib = name : builtins.concatStringsSep "" [ "$" "{" (builtins.toString name) "}" ] ;
+                        in
+                            {
+                                lib = lib ;
+                                checks.testLib =
+                                    pkgs.stdenv.mkDerivation
+                                        {
+                                            name = "test-lib";
+                                            builder = "${pkgs.bash}/bin/bash" ;
+                                            args =
+                                                [
+                                                    "-c"
+                                                    ''
+                                                        observed='${ lib "myVar" }' &&
+                                                            expected='${ builtins.concatStringsSep "" [ "$" "{" "myVar" "}" ] }' &&
+                                                            if [ "$observed" != "$expected" ]
+                                                            then
+                                                                    exit 1
+                                                            else
+                                                                ${ pkgs.coreutils }/bin/mkdir $out
+                                                            fi
+                                                    ''
+                                                ] ;
+                                        } ;
+                            } ;
+                in flake-utils.lib.eachDefaultSystem fun ;
+}
